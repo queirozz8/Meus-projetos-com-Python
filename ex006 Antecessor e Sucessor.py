dbf765abd9ef4abff3160fc39054760bdcb34b87
Henrique Queiroz <136652767+queirozz8@users.noreply.github.com>
2024-01-27 03:57:11 -0300

    Exercícios Curso em Vídeo

---
+++ ex006 Antecessor e Sucessor.py
@@ -0,0 +1,2 @@
+n1 = int(input('Digite um número: '))
+print ('O seu antecessor é: {}, e o seu sucessor é {}'.format (n1-1, n1+1))
---
+++ ex011 Real Dólar.py
@@ -0,0 +1,3 @@
+print ('Cotação do dia em que esse algoritmo foi criado: 1 dólar vale 4,92 reais')
+reais = float(input('Digite seu dinheiro em reais: '))
+print ('Você teria, com {}R$, {}$.'.format (reais, reais / 4,92))
