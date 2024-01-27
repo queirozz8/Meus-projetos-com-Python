2559341f57ace814db421526395509925a861a38
Henrique Queiroz <136652767+queirozz8@users.noreply.github.com>
2024-01-27 01:26:55 -0300

    Exercícios Curso em Vídeo

---
+++ ex004 Analisador de Caractere.py
@@ -0,0 +1,6 @@
+r1 = input('Digite algo: ')
+print('O caractere é do tipo numérico?', r1.isnumeric())
+print('O caractere é do tipo alfanumérico?', r1.isalnum())
+print('O caractere é do tipo alfabético?', r1.isalpha())
+print('O caractere está em letras maiúsculas?', r1.isupper())
+print('O caractere é um caractere ascii?', r1.isascii())
