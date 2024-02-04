51d8b272e4fd0623785f3c04df44ae6ac4d9cd69
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-02-04 03:47:25 -0300

    Delete ex011 Real Dólar.py

--- ex011 Real Dólar.py
+++
@@ -1,3 +0,0 @@
-print ('Cotação do dia em que esse algoritmo foi criado: 1 dólar vale 4,92 reais')
-reais = float(input('Digite seu dinheiro em reais: '))
-print ('Você teria, com {}R$, {}$.'.format (reais, reais / 4,92))
