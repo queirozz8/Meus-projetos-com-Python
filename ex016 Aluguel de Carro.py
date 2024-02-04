6444e219cb7b08b0a76154943c46dbd9f2681237
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-02-04 03:47:59 -0300

    Delete ex016 Aluguel de Carro.py

--- ex016 Aluguel de Carro.py
+++
@@ -1,4 +0,0 @@
-dias = int (input('Quantos dias o carro foi alugado? '))
-km = float (input('Quantos Km foram rodados? '))
-pago = (dias * 60) + (km * 0.15)
-print ('O total a se pagar é R$ {:.2f}'.format (pago))
