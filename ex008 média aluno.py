e775f43cdbf3ffbebd992bbe402dd9b14017fd94
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-02-04 03:47:12 -0300

    Delete ex008 média aluno.py

--- ex008 média aluno.py
+++
@@ -1,5 +0,0 @@
-print ('Olá, eu sou um calculador de médias de notas de escola')
-n1 = float(input('Digite a primeira nota do aluno: '))
-n2 = float(input('Digite a segunda nota do aluno: '))
-m = (n1 + n2) / 2
-print ('A média entre as notas', n1, 'e', n2, 'é', m)
