1feb82f452b817af740e3f134021bab8dcead537
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-02-06 01:59:57 -0300

    Update ex024 Analisador de nome.py

--- Exercícios Curso em Vídeo/ex024 Analisador de nome.py
+++ Exercícios Curso em Vídeo/ex024 Analisador de nome.py
@@ -1,8 +1,9 @@
-nomecompleto = str(input('Qual é o seu nome? '))
+nomecompleto = str(input('Qual é o seu nome? ')).strip()
 print('Em letras maiúsculas: {}'.format(nomecompleto.upper()))
 print('Em letras minúsculas: {}'.format(nomecompleto.lower()))
 print('Tudo junto: {}'.format(nomecompleto.replace(' ', '')))
 nomes_separados = nomecompleto.split()
 primeiro_nome = nomes_separados[0]
+print('Quantas letras tem seu nome completo: {}'.format(len(nomecompleto) - nomecompleto.count(' ')))
 print('Qual é o primeiro nome: {}'.format(primeiro_nome))
 print('Quantas letras tem o primeiro nome: {}'.format(len(primeiro_nome)))
