cdb09dc73c47a9c7cad36a1a9f5259cf379919bf
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-02-06 15:31:40 -0300

    Update ex027 O nome tem silva.py

--- Exercícios Curso em Vídeo/ex027 O nome tem silva.py
+++ Exercícios Curso em Vídeo/ex027 O nome tem silva.py
@@ -1,2 +1,7 @@
+#EN: Requests the user's full name
+#BR: Solicita o nome completo do usuário
 nome = str(input('Qual é seu nome completo? ')).strip()
+
+#EN: Look for the word silva somewhere in the full name, the program puts the full name in lowercase letters to avoid bugs as well.
+#BR: Procura a palavra silva em algum lugar do nome completo, o programa coloca o nome completo em letras minúsculas pra evitar bugs também.
 print ('Seu nome/sobrenome tem Silva? {}'.format('silva' in nome.lower()))
