3e950d7747a89de395dc6926c3c18fca40f61c2d
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-02-06 02:35:55 -0300

    Update ex026 Cidade comeca com Santo.py

--- Exercícios Curso em Vídeo/ex026 Cidade comeca com Santo.py
+++ Exercícios Curso em Vídeo/ex026 Cidade comeca com Santo.py
@@ -1,3 +1,2 @@
-nome_cidade = str(input('Digite o nome de uma cidade: '))
-comecacomsanto = nome_cidade[:5].lower() == 'santo' and len(nome_cidade) > 6
-print('O nome da sua cidade começa com a palavra Santo? {}'.format(comecacomsanto))
+cidade = str(input('Digite o nome de uma cidade: ')).strip()
+print(cidade[:5].upper() == 'SANTO')
