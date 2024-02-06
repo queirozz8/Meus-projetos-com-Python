3ad384da3b67063e21598e16755587f865a062a5
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-02-06 02:13:34 -0300

    Update ex025 Analisador de número.py

--- Exercícios Curso em Vídeo/ex025 Analisador de número.py
+++ Exercícios Curso em Vídeo/ex025 Analisador de número.py
@@ -1,5 +1,9 @@
-numero = str(input('Digite seu número de 0 até 9999: '))
-print('Unidade: {}'.format(numero[3]))
-print('Dezena: {}'.format(numero[2]))
-print('Centena: {}'.format(numero[1]))
-print('Unidade de Milhar: {}'.format(numero[0]))
+num = int(input('Digite seu número de 0 até 9999: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
