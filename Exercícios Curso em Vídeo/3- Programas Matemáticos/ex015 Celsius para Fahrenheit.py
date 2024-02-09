e9b45df2ac09b3e597a3102c70e8a87e3eb469e7
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-02-09 20:41:45 -0300

    Update and rename Exercícios Curso em Vídeo/ex015 Celsius para Fahrenheit.py to Exercícios Curso em Vídeo/3- Programas Matemáticos/ex015 Celsius para Fahrenheit.py

---
+++ Exercícios Curso em Vídeo/3- Programas Matemáticos/ex015 Celsius para Fahrenheit.py
@@ -0,0 +1,3 @@
+c = float(input('Digite a temperatura em °C: '))
+f = 9 * c / 5 + 32
+print ('A temperatura de {} °C é equivalente à {} °F.'.format (c, f))
--- Exercícios Curso em Vídeo/ex015 Celsius para Fahrenheit.py
+++
@@ -1,3 +0,0 @@
-c = float(input('Digite a temperatura em °C: '))
-f = 9 * c / 5 + 32
-print ('A temperatura de {} °C é equivalente à {} °F.'.format (c, f))
