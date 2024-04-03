d686cac09f6ac201dd760b3953f92c839271587d
Henrique Jorge de Queiroz <zeccakut@gmail.com>
2024-04-03 17:50:58 -0300

    Curso Em Vídeo Exercícios Mundo 2

---
+++ Exercícios Curso em Vídeo Mundo 2/ex39 Média de velocidade.py
@@ -0,0 +1,8 @@
+print("-="*20)
+print("     CALCULADOR DE VELOCIDADE MÉDIA")
+print("-="*20)
+distancia = float(input("Digite a distância de um ponto a outro (km): "))
+tempo = float(input("Digite o tempo em que o veículo chegou de um ponto a outro (horas): "))
+media = distancia / tempo
+print("-"*70)
+print(f"A velocidade média em que seu veículo estava era de {media}km/h.")
