b74c4fb5fb2fd1b75600565b538bd18099297d09
Henrique Queiroz <zeccakut@gmail.com>
2024-07-11 21:09:33 -0300

    Update ex095 Boletim com Listas Compostas.py

--- Exercícios Curso em Vídeo Mundo 3/2 - Listas/ex095 Boletim com Listas Compostas.py
+++ Exercícios Curso em Vídeo Mundo 3/2 - Listas/ex095 Boletim com Listas Compostas.py
@@ -23,18 +23,20 @@ print(f'{'N°':<4}{'NOME':<15}{'MÉDIA':>9}')
 print('-'*30)
 # Para cada aluno na ficha, sendo que ind_aluno é o índice do aluno:
 for ind_aluno, aluno in enumerate(ficha):
-    # Mostre o índice do aluno + 1 (enumerate trabalha começando com 0, para deixar mais fácil para os usuários, adicionamos +1 para que o primeiro aluno seja 1, não 0), o elemento 0 (que representa o nome dele)
-    # (O aluno que ele deve mostrar é ordenado pela variável aluno), e por fim o elemento 2 (a média dele):
+    # Mostre o índice do aluno + 1 (enumerate trabalha começando com 0, para deixar mais fácil para os usuários, adicionamos +1 para que o primeiro aluno seja 1, não 0), o
+    # elemento  0 (que representa o nome dele) (O aluno que ele deve mostrar é ordenado pela variável aluno), e por fim o elemento 2 (a média dele):
     print(f'{ind_aluno + 1:<4}{aluno[0]:<15}{aluno[2]:>9.2f}')
 
 while True:
     print('-'*30)
     opc = int(input('Qual aluno deseja ver as notas? (Digite o NÚMERO DO ALUNO. Digite -1 para VER O BOLETIM NOVAMENTE. Digite -2 para PARAR.): '))
     if 0 < opc <= len(ficha):
-        # Como as listas começam no 0, colocamos - 1 para que o índice esteja correto novamente, pois na verdade o usuário está digitando o valor "errado" do aluno que ele quer ver, vamos supor que ele quer 
-        # ver o primeiro aluno (que é 0, porém mostramos 1), quando ele digita 1, os dados do aluno que o software passaria seriam do aluno errado. Então corrigimos isso com o [opc - 1]:
+        # Como as listas começam no 0, colocamos - 1 para que o índice esteja correto novamente, pois na verdade o usuário está digitando  o valor "errado" do aluno que 
+        # ele quer  ver, vamos supor que ele quer ver o primeiro aluno (que é 0, porém mostramos 1), quando  ele digita 1, os dados do aluno que o software passaria 
+        # seriam do aluno errado. Então corrigimos isso com o [opc - 1]:
         print(f'As notas de {ficha[opc - 1][0]} são ', end='')
-        # Separe cada elemento da lista por vírgulas (transforme todos os elementos em strings para que isso funcione), mostre a média do aluno (Mesma técnica do - 1 já explicada anteriormente.):
+        # Separe cada elemento da lista por vírgulas (transforme todos os elementos em strings para que isso funcione), mostre a média do aluno (Mesma técnica do - 1 já 
+        # explicada anteriormente):
         print(', '.join(map(str, ficha[opc - 1][1])) + f', a média é {ficha[opc - 1][2]}', end='. ')
         # Se a média do aluno for igual ou maior que 6, então ele passou:
         if ficha[opc - 1][2] >= 6:
@@ -61,4 +63,4 @@ while True:
         break
 
 print('-'*30)
-print('>>> OBRIGADO, VOLTE SEMPRE! <<<')
+print('>>> OBRIGADO, VOLTE SEMPRE! <<<\n')
