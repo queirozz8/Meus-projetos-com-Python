ddc5f38daeaad9e1fd9da0da56daac42dd609e21
Henrique Queiroz <zeccakut@gmail.com>
2024-07-22 18:58:23 -0300

    Create Programa Principal.py

---
+++ Exercícios Curso em Vídeo Mundo 3/5 - Modularização/ex113/Programa Principal.py
@@ -0,0 +1,11 @@
+from ex113 import moeda
+
+p = float(input('Digite o preço: R$'))
+
+print(f'A metade de R${p:,} é R${moeda.metade(p):,}.')
+
+print(f'O dobro de R${p:,} é R${moeda.dobro(p):,}.')
+
+print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10)}.')
+
+print(f'Diminuindo 10%, temos R${moeda.diminuir(p, 10)}.')
