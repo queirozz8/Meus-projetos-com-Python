09186f5a439afff4ee000fee89e3d35150fea829
Henrique Queiroz <zeccakut@gmail.com>
2024-07-23 03:09:51 -0300

    Create moeda.py

---
+++ Exercícios Curso em Vídeo Mundo 3/5 - Modularização/ex114/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(preco=0, taxa=0):
+    res = preco + (preco * taxa/100)
+    return res
+
+def diminuir(preco=0, taxa=0):
+    res = preco - (preco * taxa/100)
+    return res
+    
+def dobro(preco=0):
+    res = preco * 2
+    return res
+        
+def metade(preco):
+    res = preco / 2
+    return res
+
+def moeda(preco, moeda='R$'):
+    return f'{moeda}{preco:>.2f}'.replace('.', ',')
