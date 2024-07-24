1c53068a58c15422d72fd03fa6969a5f829e4318
Henrique Queiroz <zeccakut@gmail.com>
2024-07-24 03:43:21 -0300

    Delete Exercícios Curso em Vídeo Mundo 3/5 - Modularização/ex118/utilidadesCeV/Programa Principal.py

--- Exercícios Curso em Vídeo Mundo 3/5 - Modularização/ex118/utilidadesCeV/Programa Principal.py
+++
@@ -1,6 +0,0 @@
-from utilidadesCeV import moeda, dado
-
-p = dado.leia_dinheiro('Digite o preço: R$')
-taxa_au = float(input('Quantos % de aumento? '))
-taxa_di = float(input('Quantos % de desconto? '))
-moeda.resumo(p, taxa_au, taxa_di)
