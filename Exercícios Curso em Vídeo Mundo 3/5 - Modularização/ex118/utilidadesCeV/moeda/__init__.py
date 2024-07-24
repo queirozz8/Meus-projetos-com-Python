b04387385b137e774b30935fb3fec7e33ff0a6cb
Henrique Queiroz <zeccakut@gmail.com>
2024-07-24 03:55:43 -0300

    Update __init__.py

--- Exercícios Curso em Vídeo Mundo 3/5 - Modularização/ex118/utilidadesCeV/moeda/__init__.py
+++ Exercícios Curso em Vídeo Mundo 3/5 - Modularização/ex118/utilidadesCeV/moeda/__init__.py
@@ -48,8 +48,7 @@ def moeda(preco=0, moeda='R$'):
     """
     -> Mostra o preço de forma formatada, bonita.
     preco é o preço que o usuário fornecer, se ele não fornecer nenhum preço, então o padrão é 0.
-    moeda é a moeda que vai ser mostrada antes do preço, você pode modificar ela se quiser, o padrão é R$, mas pode colocar USD$, etc.
-    """
+    moeda é a moeda que vai ser mostrada antes do preço, você pode modificar ela se quiser, o padrão é R$, mas pode colocar USD$, etc."""
     
     return f'{moeda}{preco:<.2f}'.replace('.', ',')
 
@@ -60,8 +59,7 @@ def resumo(preco=0, taxa_a=10, taxa_r=5):
     
     preco é o preço fornecido pelo usuário. Por padrão, é 0.
     taxa_a é a taxa de aumento fornecida pelo usuário. Por padrão, é 10.
-    taxa_r é a taxa de redução fornecida pelo usuário. Por padrão, é 5.
-    """
+    taxa_r é a taxa de redução fornecida pelo usuário. Por padrão, é 5."""
     
     print('-'*40)
     print('RESUMO DO VALOR'.center(40))
