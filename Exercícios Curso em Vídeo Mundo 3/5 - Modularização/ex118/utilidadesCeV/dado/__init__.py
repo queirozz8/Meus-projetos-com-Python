7122182df10318433499964851433006a1f53405
Henrique Queiroz <zeccakut@gmail.com>
2024-07-24 03:51:23 -0300

    Update __init__.py

--- Exercícios Curso em Vídeo Mundo 3/5 - Modularização/ex118/utilidadesCeV/dado/__init__.py
+++ Exercícios Curso em Vídeo Mundo 3/5 - Modularização/ex118/utilidadesCeV/dado/__init__.py
@@ -1,9 +1,12 @@
 def leia_dinheiro(msg):
     valido = False
     while not valido:
+        # A entrada do usuário é primeiramente lida como uma string, para que possamos substituir todas as ',' que o usuário digitar, por '.', para que o Python entenda:
         entrada = input(msg).strip().replace(',', '.')
+        # Se a entrada do usuário for alfanumérica (Ou seja, tem alguma letra) ou for uma string vazia, exiba uma mensagem de erro na cor vermelha:
         if entrada.isalpha() or entrada == '':
             print(f'\033[031mERRO: "{entrada}" é um preço inválido!\033[m')
+        # Caso contrário, se for realmente um número, então valido recebe True, e a função retorna a entrada, só que do tipo float agora:
         else:
             valido = True
             return float(entrada)
